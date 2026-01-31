c8bccf5cdd04678f6b27bb49c16bc5cd71f2150a
Matt Newbigging <mrnewbigging@gmail.com>
2026-01-31 16:04:25 +0000

    basic movement and gym floor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
+import { Game } from "../game";
 import "./app.scss";
 
+const game = new Game();
+
 export function App() {
-  return <div className="app">Hello World!</div>;
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // When loaded, will re-render and show start button
+    const onLoad = () => {
+      setLoaded(true);
+    };
+
+    // Call load at start
+    game.load(onLoad);
+  }, []);
+
+  function onStart() {
+    game.start();
+  }
+
+  return (
+    <div className="app">
+      {!loaded && <LoadingText />}
+      {loaded && <StartButton onClick={onStart} />}
+    </div>
+  );
+}
+
+function LoadingText() {
+  return <div className="loading-text">Loading...</div>;
+}
+
+interface StartButtonProps {
+  onClick: () => void;
+}
+
+function StartButton({ onClick }: StartButtonProps) {
+  return (
+    <div className="start-button" onClick={onClick}>
+      Start
+    </div>
+  );
 }
---
+++ src/game.ts
@@ -0,0 +1,145 @@
+import * as THREE from "three";
+import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls";
+
+export interface GameKeys {
+  w: boolean;
+  a: boolean;
+  s: boolean;
+  d: boolean;
+}
+
+export class Game {
+  private renderer: THREE.WebGLRenderer;
+  private camera = new THREE.PerspectiveCamera();
+  private scene = new THREE.Scene();
+  private clock = new THREE.Clock();
+  private controls: PointerLockControls;
+
+  private moveSpeed = 5;
+
+  private keys: GameKeys = {
+    w: false,
+    a: false,
+    s: false,
+    d: false,
+  };
+
+  constructor() {
+    // Setup
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.setupLights();
+
+    this.camera.position.set(0, 1.8, 3);
+    this.controls = new PointerLockControls(
+      this.camera,
+      this.renderer.domElement,
+    );
+
+    // Listeners
+    window.addEventListener("resize", this.onCanvasResize);
+    window.addEventListener("keydown", this.onKeyDown);
+    window.addEventListener("keyup", this.onKeyUp);
+  }
+
+  async load(onComplete: () => void) {
+    const loader = new THREE.TextureLoader();
+
+    // Gym floor
+    const gymFloorUrl = getUrl("/textures/gym_floor.png");
+    const gymFloorTexture = await loader.loadAsync(gymFloorUrl);
+    gymFloorTexture.colorSpace = THREE.SRGBColorSpace;
+    gymFloorTexture.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
+
+    const gymFloor = new THREE.Mesh(
+      new THREE.PlaneGeometry(28, 15),
+      new THREE.MeshPhysicalMaterial({
+        map: gymFloorTexture,
+        roughness: 0.45,
+        metalness: 0.0,
+      }),
+    );
+    gymFloor.rotateX(-Math.PI / 2);
+    this.scene.add(gymFloor);
+
+    onComplete();
+  }
+
+  start() {
+    document.body.appendChild(this.renderer.domElement);
+    this.onCanvasResize();
+    this.controls.lock();
+    this.update();
+  }
+
+  update = () => {
+    requestAnimationFrame(this.update);
+
+    const dt = this.clock.getDelta();
+
+    this.movePlayer(dt);
+
+    this.renderer.render(this.scene, this.camera);
+  };
+
+  private movePlayer(dt: number) {
+    const direction = new THREE.Vector3();
+    direction.z = Number(this.keys.w) - Number(this.keys.s);
+    direction.x = Number(this.keys.d) - Number(this.keys.a);
+
+    this.controls.moveForward(direction.z * this.moveSpeed * dt);
+    this.controls.moveRight(direction.x * this.moveSpeed * dt);
+  }
+
+  private setupLights() {
+    const ambientLight = new THREE.AmbientLight(undefined, 1);
+    this.scene.add(ambientLight);
+
+    const directLight = new THREE.DirectionalLight(undefined, Math.PI);
+    directLight.position.copy(new THREE.Vector3(0.75, 1, 0.75).normalize());
+    this.scene.add(directLight);
+  }
+
+  private onCanvasResize = () => {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+  };
+
+  private onKeyDown = (e: KeyboardEvent) => {
+    switch (e.key) {
+      case "w":
+        this.keys.w = true;
+        break;
+      case "a":
+        this.keys.a = true;
+        break;
+      case "s":
+        this.keys.s = true;
+        break;
+      case "d":
+        this.keys.d = true;
+        break;
+    }
+  };
+
+  private onKeyUp = (e: KeyboardEvent) => {
+    switch (e.key) {
+      case "w":
+        this.keys.w = false;
+        break;
+      case "a":
+        this.keys.a = false;
+        break;
+      case "s":
+        this.keys.s = false;
+        break;
+      case "d":
+        this.keys.d = false;
+        break;
+    }
+  };
+}
+
+function getUrl(path: string) {
+  return new URL(path, import.meta.url).href;
+}
